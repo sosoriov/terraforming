664db5e559016b17730e9e162f998fbd7ee2d5b9
Sebastian Osorio Vélez <sebastian.osoriovelez@delen.be>
2019-07-06 10:33:06 +0200

    refactor code to us Terraform 0.12 variable synt

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,12 +1,12 @@
 resource "digitalocean_kubernetes_cluster" "kubernetes_id" {
-  name    = "${var.name}"
-  region  = "${var.region}"
-  version = "${var.kubernetes_version}"
-  tags    = "${var.tags}"
+  name    = var.name
+  region  = var.region
+  version = var.kubernetes_version
+  tags    = var.tags
 
   node_pool {
-    name       = "${var.node_pool_name}"
-    size       = "${var.node_pool_size}"
-    node_count = "${var.node_pool_node_count}"
+    name       = var.node_pool_name
+    size       = var.node_pool_size
+    node_count = var.node_pool_node_count
   }
 }
--- main.tf
+++ main.tf
@@ -3,20 +3,20 @@ variable "do_token" {
 }
 
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = var.do_token
 }
 
 module "kubernetes" {
   source = "./kubernetes"
 
-  kubernetes_version = "${var.kubernetes_version}"
-  name = "${var.kubernetes_cluster_name}"
-  region = "${var.cluster_region}"
-  tags = "${var.cluster_tags}"
+  kubernetes_version = var.kubernetes_version
+  name = var.kubernetes_cluster_name
+  region = var.cluster_region
+  tags = var.cluster_tags
 
-  node_pool_name = "${var.node_pool_name}"
-  node_pool_size = "${var.node_capacity}"
-  node_pool_node_count = "${var.number_of_nodes}"
+  node_pool_name = var.node_pool_name
+  node_pool_size = var.node_capacity
+  node_pool_node_count = var.number_of_nodes
   
 }
 
